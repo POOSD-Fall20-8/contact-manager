5225357369a5b2ce7e214dd4e72350b906040224
Debian <bitnami@ip-172-26-14-107.ec2.internal>
2020-09-02 21:56:20 +0000

    add db connection test

--- API/phptest.php
+++ API/phptest.php
@@ -1,1 +1,16 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	$conn = new mysqli("localhost","dbadmin","dbpass","ContactManager");
+	if($conn->connect_error){
+		echo "error";
+	}
+	else{
+		echo "connected to db";
+	}
+	$conn->close();
+?>
+</body>
+</html>
+

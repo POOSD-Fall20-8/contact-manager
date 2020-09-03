4865abf40bff2ef9cf593527380cf4806561b386
rakomw <robbiemoore4698@gmail.com>
2020-09-03 13:49:08 -0400

    begin login functionality

--- API/phptest.php
+++ API/phptest.php
@@ -4,13 +4,30 @@
 <?php
 	$conn = new mysqli("localhost","dbadmin","dbpass","ContactManager");
 	if($conn->connect_error){
-		echo "error";
+		echo "error\n";
 	}
 	else{
-		echo "connected to db";
+		echo "connected to db\n";
+	}
+	$login = RickL;
+	$pass = COP4331;
+	$sql = "SELECT ID,firstName,lastName FROM Users where Login='" . $login . "' and Password='" . $pass . "'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0)
+	{
+		$row = $result->fetch_assoc();
+		$firstName = $row["firstName"];
+		$lastName = $row["lastName"];
+		$id = $row["ID"];
+
+		echo "Name: ".$firstName." ".$lastName"\n";
+		echo "ID: ".$id."";
+	}
+	else
+	{
+		returnWithError( "No Records Found" );
 	}
 	$conn->close();
 ?>
 </body>
 </html>
-

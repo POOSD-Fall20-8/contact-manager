976c67d8da800db8f02cf96027b1d82d244c3733
rakomw <robbiemoore4698@gmail.com>
2020-09-03 14:26:13 -0400

    update phptest

--- API/phptest.php
+++ API/phptest.php
@@ -2,32 +2,21 @@
 <html>
 <body>
 <?php
-	$conn = new mysqli("localhost","dbadmin","dbpass","ContactManager");
-	if($conn->connect_error){
-		echo "error\n";
-	}
-	else{
-		echo "connected to db\n";
-	}
-	$login = RickL;
-	$pass = COP4331;
-	$sql = "SELECT ID,firstName,lastName FROM Users where Login='" . $login . "' and Password='" . $pass . "'";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0)
-	{
-		$row = $result->fetch_assoc();
-		$firstName = $row["firstName"];
-		$lastName = $row["lastName"];
-		$id = $row["ID"];
+    $conn = new mysqli("localhost","dbadmin","dbpass","ContactManager");
+    if($conn->connect_error){
+        echo "error connecting to database\n";
+    }
+    $login = RickL;
+    $pass = COP4331;
 
-		echo "Name: ".$firstName." ".$lastName"\n";
-		echo "ID: ".$id."";
-	}
-	else
-	{
-		returnWithError( "No Records Found" );
-	}
-	$conn->close();
+    $sql = "SELECT user_id,first_name,last_name FROM users where login='" . $login. "' and password='" . $pass . "'";
+
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+        echo "Name: " . $row[first_name] . " " . $row[last_name];
+    }
+    $conn->close();
 ?>
 </body>
 </html>

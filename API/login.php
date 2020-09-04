87d509fd7ee8bc087f226e0e47a3cda858350509
rakomw <robbiemoore4698@gmail.com>
2020-09-04 12:38:59 -0400

    rearrange login.php

--- API/login.php
+++ API/login.php
@@ -1,41 +1,39 @@
 <?php
+	function getRequestInfo(){
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
-    $inData = getRequestInfo();
-    $conn = new mysqli("localhost","dbadmin","dbpass","ContactManager");
-    if($conn->connect_error){
-      returnWithError($conn->connect_error);
-    }
-    $login = $inData[login];
-    $pass = $inData[password];
+	function sendResultInfoAsJson( $obj ){
+		header('Content-type: application/json');
+		echo $obj;
+	}
 
-		$sql = "SELECT user_id,first_name,last_name FROM users where login='" . $login. "' and password='" . $pass . "'";
+	function returnWithError( $err ){
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
 
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0){
-        $row = $result->fetch_assoc();
-        returnWithInfo($row["first_name"],$row["last_name"],$row["user_id"])
-    }
-    $conn->close();
+	function returnWithInfo( $firstName, $lastName, $id ){
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
 
-		function getRequestInfo()
-		{
-			return json_decode(file_get_contents('php://input'), true);
-		}
+	$inData = getRequestInfo();
 
-		function sendResultInfoAsJson( $obj )
-		{
-			header('Content-type: application/json');
-			echo $obj;
-		}
+	$conn = new mysqli("localhost", "dbadmin", "dbpass", "ContactManager");
+	if ($conn->connect_error){
+		returnWithError( $conn->connect_error );
+	}
 
-		function returnWithError( $err )
-		{
-			$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-			sendResultInfoAsJson( $retValue );
-		}
+	$sql = "SELECT user_id,first_name,last_name FROM users where login='" . $inData["login"] . "' and password='" . $inData["password"] . "'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0){
+		$row = $result->fetch_assoc();
+		returnWithInfo($row["first_name"], $row["last_name", $row["user_id"] );
+	}
+	else{
+		returnWithError( "No Records Found" );
+	}
 
-		function returnWithInfo( $firstName, $lastName, $id ){
-			$retValue = '{"id":' . $id . ',"first":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-			sendResultInfoAsJson( $retValue );
-		}
+	$conn->close();
 ?>
--- JS/practicescript.js
+++ JS/practicescript.js
@@ -3,4 +3,22 @@ function doLogin(){
   var pass = document.getElementById("loginPassword").value;
   alert("username: " + user + " password: " + pass);
 
+  var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	var url = "http://contactmanageronline.com/API/login.php";
+  alert("2");
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.send(jsonPayload);
+
+		var jsonObject = JSON.parse( xhr.responseText );
+
+		alert(" "+jsonObject.id);
+  }
+  catch(err)
+	{
+		alert("error");
+	}
 }

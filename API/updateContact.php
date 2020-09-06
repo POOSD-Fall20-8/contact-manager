193f4443a5c85eaf848f7d490c956d2a86262bbe
rakomw <robbiemoore4698@gmail.com>
2020-09-06 18:18:08 -0400

    add shells for contact CRUD endpoints

---
+++ API/updateContact.php
@@ -0,0 +1,18 @@
+<?php
+	include 'utils.php';
+
+	$inData = getRequestInfo();
+
+	$conn = new mysqli("localhost", "dbadmin", "dbpass", "ContactManager");
+	if ($conn->connect_error){
+		returnWithError( $conn->connect_error );
+	}
+	else{
+		$sql = "";
+		$result = $conn->query($sql);
+
+
+		$conn->close();
+	}
+
+?>

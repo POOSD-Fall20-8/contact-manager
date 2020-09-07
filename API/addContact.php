03f191d1a71ab524e7846e7330bc7f85df7d6660
rakomw <robbiemoore4698@gmail.com>
2020-09-07 15:15:50 -0400

    implement add contact endpoint

--- API/addContact.php
+++ API/addContact.php
@@ -1,17 +1,40 @@
 <?php
 	include 'utils.php';
 
+	function returnWithError( $err ){
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $id ){
+		$retValue = '{"record_id":"' . $id   . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
 	$inData = getRequestInfo();
+
+	$address = $inData->address;
+	$email = $inData->email;
+	$first_name = $inData->first_name;
+	$last_name = $inData->last_name;
+	$phone = $inData->phone;
+	$user_id = $inData->user_id;
 
 	$conn = new mysqli("localhost", "dbadmin", "dbpass", "ContactManager");
 	if ($conn->connect_error){
 		returnWithError( $conn->connect_error );
 	}
 	else{
-		$sql = "";
+		$sql = "INSERT INTO contacts (address,email,first_name,last_name,phone,user_id,date_added)".
+						" VALUES '". $address . "','". $email . "', '". $first_name ."','" . $last_name ."','".
+						$phone ."','". $user_id . "', NOW())";
 		$result = $conn->query($sql);
-
-		
+		if(result == TRUE){
+			returnWithInfo($conn->insert_id);
+		}
+		else{
+			returnWithError($conn->error);
+		}
 		$conn->close();
 	}
 

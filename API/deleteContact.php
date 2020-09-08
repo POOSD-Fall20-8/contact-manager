1f2535bd3aed741a25352df5b8a96a1ad68da5f9
rakomw <robbiemoore4698@gmail.com>
2020-09-08 19:07:25 -0400

    implement delete contact endpoint

--- API/deleteContact.php
+++ API/deleteContact.php
@@ -1,17 +1,35 @@
 <?php
 	include 'utils.php';
 
+	function returnWithError( $err ){
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnSuccess(){
+		$retValue = '{"error":""}';
+		sendResultInfoAsJson($retValue);
+	}
+
 	$inData = getRequestInfo();
+
+	$record_id = $inData->record_id;
+	$user_id = $inData->user_id;
+
 
 	$conn = new mysqli("localhost", "dbadmin", "dbpass", "ContactManager");
 	if ($conn->connect_error){
 		returnWithError( $conn->connect_error );
 	}
 	else{
-		$sql = "";
+		$sql = "DELETE FROM contacts WHERE record_id='". $record_id . "' and user_id='". $user_id ."'";
 		$result = $conn->query($sql);
-
-
+		if ($conn->affected_rows > 0){
+			returnSuccess();
+		}
+		else{
+			returnWithError($conn->error);
+		}
 		$conn->close();
 	}
 

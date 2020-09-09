a08f965b4f40ab0cfde9ab41845a99ad4798d340
Debian <bitnami@ip-172-26-14-107.ec2.internal>
2020-09-09 00:38:24 +0000

    minor fixes

--- API/searchContact.php
+++ API/searchContact.php
@@ -1,22 +1,24 @@
 <?php
 	include 'utils.php';
 
+
 	function returnWithError( $err ){
 		$retValue = '{"matched":[],"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 
 	function buildJSON( $first_name,$last_name,$email, $phone,$address,$record_id ){
-		$retValue = '{"first_name":"'.$first_name.'","last_name":"'.$last_name.'",'.
-			'"email":"'.$email.'","phone":"'.$phone.'","address":"'.$address.'",'.
-			'"record_id":"'.$record_id.'","error":""}';
+		$retValue = '{"first_name":"'. $first_name .'","last_name":"'. $last_name .'",' .
+			'"email":"'. $email .'","phone":"'. $phone .'","address":"'. $address .'",'.
+			'"record_id":"'. $record_id .'","error":""}';
 		return $retValue;
 	}
 
 	function returnWithInfo($matched){
-		$retValue = '{"matched":'.$matched.',"error":"' . $err . '"}';
+		$retValue = '{"matched":'. $matched .',"error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
+
 	$inData = getRequestInfo();
 
 	$search = $inData->search;
@@ -27,22 +29,22 @@
 		returnWithError( $conn->connect_error );
 	}
 	else{
-		$sql = "SELECT first_name,last_name,email,phone,address,record_id FROM users WHERE ".
-		"first_name like '%". $search."%' OR last_name like '%". $search."%' OR " .
-		"email like '%". $search."%' OR phone like '%". $search."%' OR address like '%". $search."%' ".
-		"OR record_id='".$search."' AND user_id = '".$user_id."'";
+		$sql = "SELECT first_name,last_name,email,phone,address,record_id FROM contacts WHERE ".
+		"first_name like '%". $search ."%' OR last_name like '%". $search ."%' OR " .
+		"email like '%". $search ."%' OR phone like '%". $search ."%' OR address like '%". $search ."%' ".
+		"OR record_id='". $search ."' AND user_id = '". $user_id ."'";
 		$result = $conn->query($sql);
-		if ($result->numRows > 0){
+		if ($result->num_rows > 0){
 			$matched = '[';
 			while($row = $result->fetch_assoc()){
 				$matched .= buildJson($row[first_name],$row[last_name],$row[email],$row[phone],$row[address],$row[record_id]);
 				$matched .= ',';
 			}
-			$matched = substr_replace($matched,-1,"]");
+			$matched = substr_replace($matched,']',-1);
 			returnWithInfo($matched);
 		}
 		else {
-			returnWithError("No Records Found")
+			returnWithError("No Records Found");
 		}
 
 		$conn->close();

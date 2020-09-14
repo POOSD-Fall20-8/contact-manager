3f514208cc88195c6d8494074baf264d9e911718
travtrav484 <40413179+travtrav484@users.noreply.github.com>
2020-09-13 20:38:38 -0400

    Update updateUser.php

--- API/updateUser.php
+++ API/updateUser.php
@@ -27,14 +27,17 @@
         $sql = "UPDATE Users SET";
         if($first_name !=""){
             $sql .= "first_name = '" . $first_name ."',";
+	    echo "succesfully updated!";
         }if ($last_name != ""){
-                $sql .= "last_name = '" . $last_name ."',";
+            $sql .= "last_name = '" . $last_name ."',";
+	    echo "succesfully updated!";
         }if ($login != ""){
             $sql .= "login = '" . $login ."',";
+	    echo "succesfully updated!";
         }if ($password != ""){
             $sql .= "password = '" . $password ."',";
+	    echo "succesfully updated!";
         }
-        
     }
 	
 ?>

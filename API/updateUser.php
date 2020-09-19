617ce4d68ab14abb6b26f9196f3d749865b11190
travtrav484 <40413179+travtrav484@users.noreply.github.com>
2020-09-18 21:10:30 -0400

    Update updateUser.php

--- API/updateUser.php
+++ API/updateUser.php
@@ -14,7 +14,7 @@
 
 	$inData = getRequestInfo();
 
-	$login = $inData->login;
+	$user_id = $inData->user_id;
 	$password = $inData->password;
 	$first_name = $inData->first_name;
 	$last_name = $inData->last_name;
@@ -24,22 +24,36 @@
 		returnWithError( $conn->connect_error );
     }
     else{
-        $sql = "UPDATE Users SET";
-	$result = $conn->query($sql);
+		$sql = "UPDATE Users SET";
+		
         if($first_name !=""){
-            $sql .= "first_name = '" . $first_name ."',";
-	    echo "succesfully updated!";
+			$sql .= "first_name = '" . $first_name ."',";
+			
         }if ($last_name != ""){
-            $sql .= "last_name = '" . $last_name ."',";
-	    echo "succesfully updated!";
-        }if ($login != ""){
-            $sql .= "login = '" . $login ."',";
-	    echo "succesfully updated!";
-        }if ($password != ""){
-            $sql .= "password = '" . $password ."',";
-	    echo "succesfully updated!";
+			$sql .= "last_name = '" . $last_name ."',";
+			
         }
-	 $conn->close();
-    }
+		if ($password != ""){
+			$sql .= "password = '" . $password ."',";
+			
+		}
+		if (substr($sql,-1) == ","){
+			$sql = substr_replace($sql," ",-1);
+		}
+		else{
+			returnWithError("No Fields To Update");
+		}
+		$sql .= "WHERE user_id = '". $user_id ."'";
+		$result = $conn->query($sql);
+		if ($conn->affected_rows > 0){
+			returnWithInfo($user_id);
+		}
+		else {
+			returnWithError("Could Not Match Record");
+		}
+        $conn->close();
+	}
+	
+	
 	
 ?>

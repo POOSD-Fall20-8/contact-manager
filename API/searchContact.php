22d079bc2660d63512b6c0876a3c21815c9518ae
rakomw <robbiemoore4698@gmail.com>
2020-09-20 12:21:01 -0400

    can now search by specific field or general

--- API/searchContact.php
+++ API/searchContact.php
@@ -22,6 +22,11 @@
 	$inData = getRequestInfo();
 
 	$search = $inData->search;
+	$first_name = $inData->first_name;
+	$last_name = $inData->last_name;
+	$email = $inData->email;
+	$phone = $inData->phone;
+	$address = $inData->address;
 	$user_id = $inData->user_id;
 
 	$conn = new mysqli("localhost", "dbadmin", "dbpass", "ContactManager");
@@ -29,10 +34,19 @@
 		returnWithError( $conn->connect_error );
 	}
 	else{
-		$sql = "SELECT first_name,last_name,email,phone,address,record_id FROM contacts WHERE ".
-		"(first_name LIKE '%". $search."%' OR last_name LIKE '%". $search."%' OR " .
-		"email LIKE '%". $search."%' OR phone LIKE '%". $search."%' OR address LIKE '%". $search."%')".
-		" AND user_id='".$user_id."'";
+		if($search != ""){
+			$sql = "SELECT first_name,last_name,email,phone,address,record_id FROM contacts WHERE ".
+			"(first_name LIKE '%". $search."%' OR last_name LIKE '%". $search."%' OR " .
+			"email LIKE '%". $search."%' OR phone LIKE '%". $search."%' OR address LIKE '%". $search."%')".
+			" AND user_id='".$user_id."'";
+		}
+		else{
+			$sql = "SELECT first_name,last_name,email,phone,address,record_id FROM contacts WHERE ".
+			"(first_name LIKE '%". $first_name."%' AND last_name LIKE '%". $last_name."%' AND " .
+			"email LIKE '%". $email."%' AND phone LIKE '%". $phone."%' AND address LIKE '%". $address."%')".
+			" AND user_id='".$user_id."'";
+		}
+
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0){
 			$matched = '[';

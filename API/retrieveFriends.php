cdf66cafb244fec7780e429b5c00dfb368a41cba
rakomw <robbiemoore4698@gmail.com>
2020-09-27 04:08:35 -0400

    retrieve friend endpoint

--- API/retrieveFriends.php
+++ API/retrieveFriends.php
@@ -3,16 +3,32 @@
 
 	$inData = getRequestInfo();
 
-  //inData->whatever
+	$user_id = $inData->user_id;
 
 	$conn = new mysqli("localhost", "dbadmin", "dbpass", "ContactManager");
 	if ($conn->connect_error){
 		returnWithError( $conn->connect_error );
 	}
 	else{
-    // $sql = whatever
+    $sql = "SELECT friend_id FROM friends WHERE user_id='".$user_id."'";
 		$result = $conn->query($sql);
-		// conditions, return info or return error
+		if ($result->num_rows > 0){
+			$friends = '[';
+			while($row = $result->fetch_assoc()){
+				$sql = "SELECT login,first_name,last_name FROM users WHERE user_id='".$friend_id."'";
+				$friendResult = $conn->query($sql);
+				if($friendResult->num_rows > 0){
+					$friendRow = $friendResult->fetch_assoc();
+					$friends .= buildUserJSON($friendRow[login],$friendRow[first_name],$friendRow[last_name],$friend_id);
+					$friends .= ',';
+				}
+			}
+			$friends = substr_replace($friends,']',-1);
+			returnWithInfo('{"friends":'.$friends.'}');
+		}
+		else {
+			returnWithInfo('"friends":[]');
+		}
 		$conn->close();
 	}
 

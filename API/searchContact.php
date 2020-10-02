9bd3765dca44ea5b541deb63e4fdfa23e2ec3eda
rakomw <robbiemoore4698@gmail.com>
2020-10-02 16:27:22 -0400

    enable retrieving a contact by the record id

--- API/searchContact.php
+++ API/searchContact.php
@@ -9,9 +9,10 @@
 	else{
 		$search = $conn->real_escape_string($inData->search);
 		$user_id = $conn->real_escape_string($inData->user_id);
+		$record_id = $conn->real_escape_string($inData->record_id);
 		$sql = "SELECT first_name,last_name,email,phone,address,record_id FROM contacts WHERE ".
 		"(CONCAT_WS(' ',first_name,last_name) LIKE '%". $search."%' OR email LIKE '%". $search."%' OR " .
-		"phone LIKE '%". $search."%' OR address LIKE '%". $search."%') AND user_id='".$user_id."'";
+		"phone LIKE '%". $search."%' OR address LIKE '%". $search."%' OR record_id='".$record_id."') AND user_id='".$user_id."'";
 
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0){
